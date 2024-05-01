ca27fed8c7441971d83096ecf974044dfc4bf410
Tran Dang Duat <23020023@vnu.edu.vn>
2024-05-01 22:23:01 +0700

    First commit

---
+++ nerdbot.py
@@ -0,0 +1,20 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+TOKEN = '7184210203:AAHh1ukQlfynmIPkgDb-6VIaF5Al0wjgfFc'
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(text = 'xin chao du ma ' + update.effective_chat.first_name + "🤓☝️", chat_id = update.effective_chat.id);
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+    
+    application.add_handler(CommandHandler('hello', hello))
+    
+    application.run_polling()
